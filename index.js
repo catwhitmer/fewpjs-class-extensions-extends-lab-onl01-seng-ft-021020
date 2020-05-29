84ce1d004ecf01ef9296287bae07e09e8527ba38
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-29 15:49:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Triangle extends Polygon {
 class Square extends Polygon {
   get isValid() {
     const [side1, side2, side3, side4] = this.sides;
-    return side1 === side2 && side1 === side3 && side 1 === side4 ? true : false;
+    return side1 === side2 && side1 === side3 && side1 === side4 ? true : false;
   }
   get area() {
     const side = this.sides[0];
